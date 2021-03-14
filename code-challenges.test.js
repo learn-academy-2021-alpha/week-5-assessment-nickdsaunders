3d9ef086d510f8697d7f783693538de853e131c8
nickdsaunders <>
2021-03-14 15:06:03 -0700

    finished challenges

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -17,35 +17,74 @@
 
 // a) Create a test with an expect statement using the variable provided.
 
+describe("when codedMessage is called", () => {
+    it ("returns a coded message that converts 'a' to 4, 'e' to 3, 'i' to 1, and 'o' to 0", () => {
+        
+        var secretCodeWord1 = "lackadaisical"
+        var secretCodeWord2 = "gobbledygook"
+
+        expect(codedMessage(secretCodeWord1)).toEqual("l4ck4d41s1c4l")
+        expect(codedMessage(secretCodeWord2)).toEqual("g0bbl3dyg00k")
+    })
+})
+
 var secretCodeWord1 = "lackadaisical"
 // Expected output: "l4ck4d41s1c4l"
 var secretCodeWord2 = "gobbledygook"
 // Expected output: "g0bbl3dyg00k"
 
-
-
-
-
 // b) Create the function that makes the test pass.
 
-
-
+// create a function that takes a string as an argument
+const codedMessage = (str) => {
+    //convert a to 4, e to 3, i to 1, and o to 0
+    // created an object to store the variables to change and their new values
+    var mapObj = {
+        a:4,
+        e:3, 
+        i:1,
+        o:0
+    }
+    // function that finds the values and replaces with the matched values.
+    strNew = str.replace(/a|e|i|o/gi, (matched) => {
+        return mapObj [matched];
+    })
+    return strNew
+    
+}
+// return str.replace(/a/g, 4) && str.replace(/i/g, 1)
+// console.log(codedMessage(secretCodeWord1))
+// console.log(codedMessage(secretCodeWord2))
 
 
 // --------------------2) Create a function that takes in an array and returns all the words that contain the letter a.
 
 // a) Create a test with an expect statement using the variable provided.
 
+describe("when wordsWithA is called", () => {
+    it ("returns all the words that contain the letter a", () => {
+        
+        var arrayOfWords = ["Apple", "Banana", "Plum", "Cherry", "Kiwi", "Peach"]
+
+        expect(wordsWithA(arrayOfWords)).toEqual(["Apple", "Banana", "Peach"])
+    })
+})
+
 var arrayOfWords = ["Apple", "Banana", "Plum", "Cherry", "Kiwi", "Peach"]
-// Expected output: "Apple" "Banana" "Peach"
-
-
-
-
+// Expected output: ["Apple", "Banana", "Peach"]
 
 // b) Create the function that makes the test pass.
 
-
+// create function that takes an array as an argument
+const wordsWithA = (arr) => {
+    // filter the array
+    let newArr = arr.filter(value => {
+        // return the words that include the letter a
+        return value.includes('A') || value.includes('a')
+    })
+    return newArr
+}
+// console.log(wordsWithA(arrayOfWords))
 
 
 
@@ -53,15 +92,60 @@ var arrayOfWords = ["Apple", "Banana", "Plum", "Cherry", "Kiwi", "Peach"]
 
 // a) Create a test with an expect statement using the variable provided.
 
-var hand1 = [5, 5, 5, 3, 3]
-// Expected output: true
-var hand2 = [5, 5, 3, 3, 4]
-// Expected output: false
-var hand3 = [5, 5, 5, 5, 4]
-// Expected output: false
+describe("when isFullHouse is called", () => {
+    it ("returns whether or not the array is a full house", () => {
+        
+        var hand1 = [5, 5, 5, 3, 3]
+        // Expected output: true
+        var hand2 = [5, 5, 3, 3, 4]
+        // Expected output: false
+        var hand3 = [5, 5, 5, 5, 4]
+        // Expected output: false
 
+        expect(isFullHouse(hand1)).toEqual(true)
+        expect(isFullHouse(hand2)).toEqual(false)
+        expect(isFullHouse(hand3)).toEqual(false)
+    })
+})
 
+    var hand1 = [5, 5, 5, 3, 3]
 
+    var hand2 = [5, 5, 3, 3, 4]
+
+    var hand3 = [5, 5, 5, 5, 4]
 
 
 // b) Create the function that makes the test pass.
+
+// create a function that takes an array as an argument
+const isFullHouse = (arr) => {
+    // create an object that holds the count of each of the value
+    let countObj = {}
+    for(let x of arr){
+    // checks the count of each value in the array
+      countObj[x] = (countObj[x] || 0) + 1;
+    }
+    // create local variable to store the object of values and the count for each
+    let vals = Object.values(countObj);
+    // checks if there are 2 of one value and 3 of another
+    if((vals[0] === 2 && vals[1] === 3) || (vals[1] === 2 && vals[0] === 3)){
+      return true;
+    }
+    return false;
+  }
+// console.log(isFullHouse(hand1))
+// console.log(isFullHouse(hand2))
+// console.log(isFullHouse(hand3))
+
+// iterating through array check for count of those values
+// storing in object makes sense
+// need to keep track of how many there are 
+// starting with first chaacter first index and check against that instance , add that to object each time you come acrsoos and update 
+
+// find two unique values and check to see if there is 3 of one  two of the other
+//isolate one char check for how many times it appears then use another char and see how many times that appearas
+// use every? 
+// 
+
+// if theres 3 of something and 2 of something then full house is true
+// returns a boolean
--- rails-questions.md
+++ rails-questions.md
@@ -2,22 +2,43 @@
 
 1. What is the command to create a new Rails application with a Postgres database?
 
+On the terminal you would type: 
+rails new my_rails_app -d postgresql -T
 
 
 2. Oops, I forgot to add a foreign key to my model. Describe the steps to remedy this mistake.
 
+You will need to create a migration to update the database.
+- In the terminal $ rails g migration add_column
+-In the migration directory open the file that was just created and update the def change method to look like this:
+
+def change
+    :table_name, :modelname_id, :integer
+end
+
+-save the file and then in the terminal $ rails db:migrate
+
+-The database and schema file should now be updated to contain the forign key!
 
 
 3. What is the command to generate a Rails model for a person table with columns named shirt, pants, and shoes?
 
+rails generate model Person shirt:string pants:string shoes:string
+
+If data type was not string you could specify another data type in place of string after each column name.
 
 
 4. What is the naming convention for generating a Rails model? What is the naming convention for the table that is generated with this command?
 
+The name of the model is uppercase and singular. I think PascalCase also.
+The database table is plural and snake_case.
 
 
 5. A Rails application is going to have two models. One is called Library and one is called Book. A library will have many books and a book will belong to a library. Which model would contain the foreign key? What would the foreign key column be named?
 
+The books will contain the foreign key, the column will be named library_id
 
 
 6. What is the schema file in a Rails application? Can you modify the schema directly? Why or why not?
+
+The schema file is a ruby file that is a blueprint for what the tables should look like in the database. You should not modify the schema file directly because they are not designed to be edited, they just represent the current state of the database.
